7aeb574437c8634b8d2eeccbe47fbbe7695adacf
Benjamin Tran Dinh <benjamin@mapbox.com>
2017-03-15 00:43:41 -0700

    Nice modality buttons.

--- src/components/ModalityButtons.js
+++ src/components/ModalityButtons.js
@@ -4,7 +4,28 @@ class ModalityButtons extends Component {
   render() {
     return (
       <div className='directions-modality color-white h72 px42 p18 w420 flex-parent flex-parent--row flex-parent--center-main flex-parent--center-cross'>
-        car | bike | walk
+
+        <div
+          className={'w42 h42 m6 round-full flex-parent flex-parent--center-main flex-parent--center-cross ' + (this.props.modality === 'driving' ? 'bg-darken10' : '')}
+          onClick={() => this.props.onSetModality('driving')}
+        >
+          <svg className={'icon ' + (this.props.modality === 'driving' ? 'color-white' : 'color-lighten50')}><use xlinkHref='#icon-car'></use></svg>
+        </div>
+
+        <div
+          className={'w42 h42 m6 round-full flex-parent flex-parent--center-main flex-parent--center-cross ' + (this.props.modality === 'biking' ? 'bg-darken10' : '')}
+          onClick={() => this.props.onSetModality('biking')}
+        >
+          <svg className={'icon ' + (this.props.modality === 'biking' ? 'color-white' : 'color-lighten50')}><use xlinkHref='#icon-bike'></use></svg>
+        </div>
+
+        <div
+          className={'w42 h42 m6 round-full flex-parent flex-parent--center-main flex-parent--center-cross ' + (this.props.modality === 'walking' ? 'bg-darken10' : '')}
+          onClick={() => this.props.onSetModality('walking')}
+        >
+          <svg className={'icon ' + (this.props.modality === 'walking' ? 'color-white' : 'color-lighten50')}><use xlinkHref='#icon-walk'></use></svg>
+        </div>
+
       </div>
     );
   }
