3304150c04a47ed7f2b9e0dab219da04f972cb31
Benjamin TD <benjamin.tdm@gmail.com>
2019-04-12 18:27:10 +0200

    service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { defaultState } from "./reducers/index";
 import apiCaller from "./middlewares/apiCaller";
 import reducers from "./reducers/index";
+import * as serviceWorker from "./serviceWorker";
 
 import App from "./components/App";
 
@@ -45,3 +46,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
