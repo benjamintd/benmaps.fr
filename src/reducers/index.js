17d11c883e7aaaeb72481994daea271e50c3b7f1
Benjamin TD <benjamin.tdm@gmail.com>
2019-04-19 12:28:05 +0200

    fix tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import { appReducer, defaultAppState } from "./appReducer";
 import { combineReducers } from "redux";
+import { createBrowserHistory } from "history";
 import { connectRouter } from "connected-react-router";
 
-const createRootReducer = history =>
-  combineReducers({
+const createRootReducer = history => {
+  if (!history) history = createBrowserHistory();
+  return combineReducers({
     router: connectRouter(history),
     app: appReducer
   });
+};
 
 const defaultState = {
   app: defaultAppState
